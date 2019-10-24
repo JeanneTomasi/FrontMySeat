383909290980daf26c2e9080e3e92c5ca51d00e8
JeanneTomasi <jeanne.zoe.tomasi@gmail.com>
2019-10-24 10:27:01 +0200

    form.ts en cours

--- BookMySeat/src/app/collectivity/collectivity-form/collectivity-form.component.ts
+++ BookMySeat/src/app/collectivity/collectivity-form/collectivity-form.component.ts
@@ -1,5 +1,6 @@
+import { Collectivity } from './../../../models/collectivity';
 import { CollectivityService } from './../../../services/collectivity.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute, Router, Params } from '@angular/router';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
@@ -12,10 +13,13 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 export class CollectivityFormComponent implements OnInit {
 
+  @Input() collectivityData = { nom: '', pattes: 0, coolitudeSurVingt: 0 };
+
   form: FormGroup;
-  collectivity: any;
-  id: any;
+  collectivity: Collectivity;
+  id: number;
   editMode: any;
+
   constructor(private service: CollectivityService, private activatedRoute: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
@@ -24,42 +28,14 @@ export class CollectivityFormComponent implements OnInit {
       name: new FormControl(null, Validators.required),
       operation_area: new FormControl(null, Validators.required),
     });
-
-    this.activatedRoute.params.subscribe((param: Params) => {
-      // tslint:disable-next-line:no-string-literal
-      this.id = param['id'];
-      if (this.id) {
-        this.service.getOne(this.id).subscribe((response: any) => {
-          this.form.setValue(response);
-        });
-      }
-    });
-
-    this.editMode = this.service.editMode;
   }
+
 
   add() {
-    this.service.add(this.form.value).subscribe(response => {
-      this.service.collectivities.push(response.body);
-      this.form.reset();
+    this.service.add(this.collectivityData).subscribe((result) => {
+      this.router.navigate(['/animal-details/']);
+    }, (err) => {
+      console.log(err);
     });
   }
-
-
-  findIndexToUpdate(item) {
-    return item.id === this;
-  }
-
-  update(id) {
-    this.service.update(this.form.value).subscribe((response: any) => {
-      this.collectivity = this.service.collectivities.find(this.findIndexToUpdate, response.body.id);
-      const index = this.service.collectivities.indexOf(this.collectivity);
-      this.service.collectivities.splice(index, 1, response.body);
-      this.editMode = false;
-      this.service.editMode = false;
-      this.router.navigate(['/collectivity/form']);
-      this.form.reset();
-    });
-  }
-
 }
