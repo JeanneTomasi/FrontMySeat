2e6563877b852c85303fe95bd497cbe637dabf03
JeanneTomasi <jeanne.zoe.tomasi@gmail.com>
2019-10-25 12:53:19 +0200

    réglages liste formulaires - json ignore properties

--- BookMySeat/src/app/crud/seatreservation/seatreservation-form/seatreservation-form.component.ts
+++ BookMySeat/src/app/crud/seatreservation/seatreservation-form/seatreservation-form.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-
+import { SeatReservationService } from './../../../../services/seatreservation.service';
+import { SeatService } from './../../../../services/seat.service';
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Seat } from 'src/models/seat';
+import { SeatReservation } from 'src/models/seatreservation';
 @Component({
   selector: 'app-seatreservation-form',
   templateUrl: './seatreservation-form.component.html',
@@ -7,9 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SeatreservationFormComponent implements OnInit {
 
-  constructor() { }
+
+
+  @Input() reservationData = { id_seat_reservation: 0, starting_time: '', release_time: '', seat: null };
+
+  seat: Seat;
+  seats: Seat[];
+  reservations: SeatReservation[];
+  reservation: SeatReservation;
+
+  // tslint:disable-next-line:max-line-length
+  constructor(private service: SeatReservationService, private serviceS: SeatService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
+    this.serviceS.findAll().subscribe((value: Seat[]) => this.seats = value);
   }
 
+  chargeVehicle(id) {
+    this.serviceS.getById(id).subscribe((value: Seat) => this.seat = value);
+  }
+
+  add() {
+    this.reservationData.seat = this.seat;
+    this.reservation = this.reservationData;
+    this.service.add(this.reservation).subscribe((result) => {
+      this.router.navigate(['/reservation/list']);
+    });
+  }
 }
--- BookMySeat/src/app/crud/seatreservation/seatreservation-list/seatreservation-list.component.ts
+++ BookMySeat/src/app/crud/seatreservation/seatreservation-list/seatreservation-list.component.ts
@@ -1,5 +1,8 @@
+import { SeatReservationService } from './../../../../services/seatreservation.service';
+import { SeatReservation } from './../../../../models/seatreservation';
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { Seat } from 'src/models/seat';
 @Component({
   selector: 'app-seatreservation-list',
   templateUrl: './seatreservation-list.component.html',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SeatreservationListComponent implements OnInit {
 
-  constructor() { }
+  reservations: SeatReservation[] = [];
+  reservation: SeatReservation;
+
+  constructor(private service: SeatReservationService, private router: Router) { }
 
   ngOnInit() {
+    this.findAll();
   }
+
+  findAll() {
+    this.service.findAll().subscribe((value: SeatReservation[]) => this.reservations = value);
+  }
+
+  add() {
+    this.router.navigate(['/reservation/form']);
+  }
+
+  delete(id) {
+    this.service.delete(id)
+      .subscribe(res => {
+        this.findAll();
+      }, (err) => {
+        console.log(err);
+      }
+      );
+  }
+
+  edit(id) {
+    this.router.navigate(['/reservation/edit', id]);
+    // this.service.editMode = true;
+  }
+
 
 }
