8e88c851870df11abbc80745e131ba668bd8d467
Vincent-Ruby <vince.ruby@gmail.com>
2019-10-25 14:19:06 +0200

    les elements apparaissent

--- BookMySeat/src/app/accueil/trafic/trafic.component.ts
+++ BookMySeat/src/app/accueil/trafic/trafic.component.ts
@@ -1,20 +1,14 @@
-import { Line } from './../../../models/line';
 import { Component, OnInit } from '@angular/core';
-
-let l1: Line = new Line();
-l1.name = '30';
-let l2: Line = new Line();
-l2.name = '42';
-export interface bus{
+export interface bus {
   id_bus: number;
   line: string;
   placesLeft: number;
 }
-const listebus: bus[] = [
-  {id_bus: 1,  line: 'l1',  placesLeft: 4},
-  {id_bus: 2,  line: 'l2',  placesLeft: 2},
-  {id_bus: 3,  line: 'l1',  placesLeft: 0},
-  {id_bus: 4,  line: 'l2',  placesLeft: 0}
+const LIST_BUS: bus[] = [
+  { id_bus: 1, line: 'l1', placesLeft: 4 },
+  { id_bus: 2, line: 'l2', placesLeft: 2 },
+  { id_bus: 3, line: 'l1', placesLeft: 0 },
+  { id_bus: 4, line: 'l2', placesLeft: 0 }
 ];
 @Component({
   selector: 'app-trafic',
@@ -23,12 +17,11 @@ const listebus: bus[] = [
 })
 
 export class TraficComponent implements OnInit {
+  displayedRows: string[] = ['id_bus', 'line', 'placesLeft'];
+  dataSource = LIST_BUS;
+  constructor() { }
 
-  constructor() {}
-  
   ngOnInit() {
   }
-  displayedRows: string[] = ['line', 'placesLeft'];
-  vehicles = listebus;
 
 }
