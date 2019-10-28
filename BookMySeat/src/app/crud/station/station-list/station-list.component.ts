05f34640c4222e2aa236456c976fe73fe424ce88
JeanneTomasi <jeanne.zoe.tomasi@gmail.com>
2019-10-28 12:11:08 +0100

    correction formulaires utilisateur

--- BookMySeat/src/app/crud/station/station-list/station-list.component.ts
+++ BookMySeat/src/app/crud/station/station-list/station-list.component.ts
@@ -1,4 +1,7 @@
+import { Router } from '@angular/router';
+import { StationService } from './../../../../services/station.service';
 import { Component, OnInit } from '@angular/core';
+import { Station } from 'src/models/station';
 
 @Component({
   selector: 'app-station-list',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StationListComponent implements OnInit {
 
-  constructor() { }
+  stations: Station[] = [];
+  station: Station;
+
+  constructor(private service: StationService, private router: Router) { }
 
   ngOnInit() {
+    this.findAll();
+  }
+
+  findAll() {
+    this.service.findAll().subscribe((value: Station[]) => this.stations = value);
+  }
+
+  add() {
+    this.router.navigate(['/station/form']);
+  }
+
+  delete(id) {
+    this.service.delete(id)
+      .subscribe(res => {
+        this.findAll();
+      }, (err) => {
+        console.log(err);
+      }
+      );
+  }
+
+  edit(id) {
+    this.router.navigate(['/station/edit', id]);
   }
 
 }
--- BookMySeat/src/app/crud/transportenterprise/transportenterprise-edit/transportenterprise-edit.component.ts
+++ BookMySeat/src/app/crud/transportenterprise/transportenterprise-edit/transportenterprise-edit.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { CollectivityService } from './../../../../services/collectivity.service';
+import { Collectivity } from 'src/models/collectivity';
+import { ActivatedRoute, Router } from '@angular/router';
+import { TransportEnterpriseService } from './../../../../services/transportenterprise.service';
+import { TransportEnterprise } from 'src/models/transportenterprise';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-transportenterprise-edit',
@@ -7,9 +12,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TransportenterpriseEditComponent implements OnInit {
 
-  constructor() { }
+  collectivity: Collectivity;
+  @Input() tEData = { id_transportEntreprise: 0, name: '', collectivity: this.collectivity };
+
+  tE: TransportEnterprise;
+  tEs: TransportEnterprise[];
+
+  // tslint:disable-next-line:max-line-length
+  constructor(private service: TransportEnterpriseService, private service2: CollectivityService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
+    this.service2.getById(1).subscribe((data: Collectivity) => {
+      this.collectivity = data;
+    });
+
+
+    // tslint:disable-next-line:no-string-literal
+    this.service.getById(this.route.snapshot.params['id']).subscribe((data: TransportEnterprise) => {
+      console.log(data);
+      this.tEData = data;
+    });
+  }
+
+  update() {
+    this.tE = this.tEData;
+    this.service.update(this.tE).subscribe((result) => {
+      this.router.navigate(['/transportEnterprise/list']);
+    });
   }
 
 }
