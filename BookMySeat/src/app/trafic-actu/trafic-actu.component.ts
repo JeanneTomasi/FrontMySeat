97968c4b34c3cc31a09ad256dec9812440f8618d
Vincent-Ruby <vince.ruby@gmail.com>
2019-10-28 16:18:15 +0100

    travail sur la page le trafc

--- BookMySeat/src/app/trafic-actu/trafic-actu.component.ts
+++ BookMySeat/src/app/trafic-actu/trafic-actu.component.ts
@@ -1,4 +1,7 @@
+import { Observable } from 'rxjs';
+import { Vehicle } from './../../models/vehicle';
 import { Component, OnInit } from '@angular/core';
+import { VehicleService } from 'src/services/vehicle.service';
 
 @Component({
   selector: 'app-trafic-actu',
@@ -7,9 +10,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TraficActuComponent implements OnInit {
 
-  constructor() { }
+  dataSource: Observable<Vehicle[]>;
+  columnDisplayed: String[] = ['line', 'station', 'placesLeft'];
+
+  constructor(private vehicleService: VehicleService) { }
 
   ngOnInit() {
+    this.dataSource = this.vehicleService.findAll();
   }
 
 }
