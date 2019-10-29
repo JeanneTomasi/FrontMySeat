6364186b6623e13b1ba5d86bbbcbf68a47bcc52b
Vincent-Ruby <vince.ruby@gmail.com>
2019-10-29 01:12:36 +0100

    si peu de temps...

--- BookMySeat/src/app/reserver/reserver.component.ts
+++ BookMySeat/src/app/reserver/reserver.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { LineService } from 'src/services/line.service';
+import { Line } from 'src/models/line';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-reserver',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReserverComponent implements OnInit {
 
-  constructor() { }
+  @Input() lineData = { id_line: 0, name: '', localisation: '' };
+  line: Line;
+  lines: Observable<Line[]>;
+
+
+  constructor(private lineService: LineService) { }
 
   ngOnInit() {
+    this.lines = this.lineService.findAll();
+  }
+
+  toBus() {
+    this.line = this.lineData;
   }
 
 }
